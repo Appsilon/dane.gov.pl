26c3d7da7c7474667dcb59d27d21c5fefb9210df
Krystian Igras <krystian8207@gmail.com>
2018-10-27 14:27:09 +0200

    Add final call for validation.

--- lista znaków towarowych/validations.R
+++ lista znaków towarowych/validations.R
@@ -1,16 +1,17 @@
 library(readr)
 library(assertr)
 library(dplyr)
-listaZnakowTowarowych <- read_delim("data/2018_10_26_listaZnakowTowarowych.csv", ";", escape_double = FALSE, trim_ws = TRUE)
-
 validator <- Validator$new()
 
+listaZnakowTowarowych <- read_delim("data/2018_10_26_listaZnakowTowarowych.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+
 listaZnakowTowarowych %>%
-  chain_start() %>%
   verify(title = "Zmienna 'Data_Zgloszenia' posiada klasę 'Date'",
          class(Data_Zgloszenia) == "Date") %>%
   verify(title = "Zmienna 'Data_Publikacji_BUP' posiada klasę 'Date'",
          class(Data_Publikacji_BUP) == "Date") %>%
   verify(title = "Zmienna 'Data_Publikacji_WUP' posiada klasę 'Date'",
          class(Data_Publikacji_WUP) == "Date") %>%
-  validator$add_validations(., "listaZnakowTowarowych")
+  validator$add_validations("listaZnakowTowarowych")
+
+validator$get_validations(type = "json")
