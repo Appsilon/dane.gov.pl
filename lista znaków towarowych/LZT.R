7a7fd88b780866bfe07057243f8bc4a37ec7383e
Michał <michalmaj116@gmail.com>
2018-10-27 13:47:03 +0200

    Remove function

--- lista znaków towarowych/LZT.R
+++ lista znaków towarowych/LZT.R
@@ -5,17 +5,12 @@ listaZnakowTowarowych <- read_delim("data/2018_10_26_listaZnakowTowarowych.csv",
 
 validator <- Validator$new()
 
-validation_rules <- function(data) {
-  data %>%
-    chain_start() %>%
-    verify(title = "Zmienna 'Data_Zgloszenia' posiada klasę 'Date'",
-           class(Data_Zgloszenia) == "Date") %>%
-    verify(title = "Zmienna 'Data_Publikacji_BUP' posiada klasę 'Date'",
-           class(Data_Publikacji_BUP) == "Date") %>%
-    verify(title = "Zmienna 'Data_Publikacji_WUP' posiada klasę 'Date'",
-           class(Data_Publikacji_WUP) == "Date")
-}
-
 listaZnakowTowarowych %>%
-  validation_rules() %>%
+  chain_start() %>%
+  verify(title = "Zmienna 'Data_Zgloszenia' posiada klasę 'Date'",
+         class(Data_Zgloszenia) == "Date") %>%
+  verify(title = "Zmienna 'Data_Publikacji_BUP' posiada klasę 'Date'",
+         class(Data_Publikacji_BUP) == "Date") %>%
+  verify(title = "Zmienna 'Data_Publikacji_WUP' posiada klasę 'Date'",
+         class(Data_Publikacji_WUP) == "Date") %>%
   validator$add_validations(., "listaZnakowTowarowych")
