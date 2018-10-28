1c59a66e0a5eb0051c09a390350d25c82af47f31
Krystian Igras <krystian8207@gmail.com>
2018-10-28 14:51:58 +0100

    Extended description.

--- utils.R
+++ utils.R
@@ -12,18 +12,21 @@ attr(check_date_format,"assertr_vectorized") <- TRUE
 
 sum_up <- function(validation_result) {
   disp_summary <- function(data_row) {
+    additional_info <- ""
     if (data_row$result == "Passed") {
       color <- crayon::green
       result <- "Poprawny!"
     } else {
       color <- crayon::red
       result <- "Niepoprawny!"
+      if (data_row$is_obligate) {
+        additional_info <- " Reguła kluczowa do przeprowadzenia dalszej walidacji."
+      }
     }
-    cat(data_row$title, ". Wynik walidacji: ", color(result), "\n", sep = "")
+    cat(data_row$title, ". Wynik walidacji: ", color(result), additional_info, "\n", sep = "")
   }
 
   purrr::walk(1:nrow(validation_result), ~ disp_summary(validation_result[., ]))
 }
 
 validator <- Validator$new()
-
